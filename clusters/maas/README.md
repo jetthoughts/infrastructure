e44d0267fe2f2034e60694da33cb1018eb3084e4
Michael Nikitochkin <miry.sof@gmail.com>
2017-07-08 21:44:55 +0200

    Added maas setup.

--- clusters/maas/README.md
+++ clusters/maas/README.md
@@ -5,12 +5,17 @@
 1. Download and burn image Ubuntu Classic Server from https://ubuntu-pi-flavour-maker.org/download/
 2. Upgrade packages and setup WiFi step by step:
 
-```
+```shell
 $ terraform apply -target=null_resource.upgrade-packages -var 'server_ip=10.0.0.1'
 $ terraform apply -target=null_resource.set-hostname -var 'server_ip=10.0.0.1' -var 'server_hostname=maas'
 $ terraform apply -target=null_resource.wifi -var 'server_ip=10.0.0.1' -var 'wlan_ssid=FreeWiFi' -var 'wlan_psk=ChangeMe'
+$ terraform apply -target=null_resource.zram -var 'server_ip=10.0.0.1'
 ```
 
 ## Install Maas packages
 
-TODO
+```shell
+$ terraform apply -target=null_resource.maas-packages -var 'server_ip=10.0.0.1 
+$ terraform apply -target=null_resource.maas-admin -var 'server_ip=10.0.0.1 
+$ terraform apply -target=null_resource.maas-eth-network -var 'server_ip=10.0.0.1 
+```
--- clusters/maas/maas.tf
+++ clusters/maas/maas.tf
@@ -1,21 +1,80 @@
+resource "null_resource" "zram" {
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    host = "${var.server_ip}"
+  }
+
+  provisioner "file" {
+    source      = "data/zram"
+    destination = "/tmp/zram"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cat /tmp/zram | sudo tee /etc/rc.local",
+      "sudo bash /tmp/zram",
+      "sudo reboot"
+    ]
+  }
+}
 
 # https://github.com/madeden/blogposts/blob/master/k8s-gpu-cluster/10-install-maas.md
+# Maas Devel: https://docs.ubuntu.com/maas/devel/en/release-notes
 resource "null_resource" "maas-packages" {
 
-  depends_on = ["null_resource.wifi"]
+  depends_on = ["null_resource.wifi", "null_resource.zram"]
 
   connection {
     type = "ssh"
     user = "ubuntu"
     host = "${var.server_ip}"
   }
-  
 
+  provisioner "file" {
+    source      = "data/rc.local"
+    destination = "/tmp/rc.local"
+  }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-add-repository -yu ppa:maas/stable",
-      "DEBIAN_FRONTEND=noninteractive sudo apt install -yqq --no-install-recommends maas bzr isc-dhcp-server wakeonlan amtterm wsmancli zram-config"
+      "sudo apt-add-repository -yu ppa:maas/next",
+      "DEBIAN_FRONTEND=noninteractive sudo apt install -yqq --no-install-recommends maas bzr isc-dhcp-server wakeonlan amtterm wsmancli zram-config maas-region-controller ntp",
+      "sudo reboot"
     ]
   }
 }
+
+resource "null_resource" "maas-eth-network" {
+  depends_on = ["null_resource.maas-packages"]
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    host = "${var.server_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo \"allow-hotplug eth0\niface eth0 inet static\n  address ${var.private_ip}\n  netmask ${var.private_netmask}\" | sudo tee /etc/network/interfaces.d/01-eth0.cfg",
+      "sudo reboot"
+    ]
+  }
+}
+
+resource "null_resource" "maas-admin" {
+  depends_on = ["null_resource.maas-packages"]
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    host = "${var.server_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo maas createadmin --username=${var.maas_admin["username"]} --email=${var.maas_admin["email"]} --password=${var.maas_admin["password"]}"
+    ]
+  }
+}
+
