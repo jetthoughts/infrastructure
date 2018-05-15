79a3b8eef544635e1b4c435b83e5741a23042d1e
Michael Nikitochkin <miry.sof@gmail.com>
2018-05-15 09:08:08 +0200

    K8S: Generate user certificates.

--- modules/k8s_master/certificates/data/generate.sh
+++ modules/k8s_master/certificates/data/generate.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+name=$1
+shift
 version=$1
 shift
 output=$1
@@ -9,3 +11,4 @@ shift
 sans=$*
 
 docker run --rm -v $output:/etc/kubernetes/pki miry/kubernetes:$version /bin/kubeadm alpha phase certs all --apiserver-cert-extra-sans "${sans}"
+docker run --rm -v $output:/etc/kubernetes/pki miry/kubernetes:$version /bin/kubeadm alpha phase kubeconfig user --client-name="${name}"
