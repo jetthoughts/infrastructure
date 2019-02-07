d0338f8ee6903b581f4bc181e76361adbf848e03
Michael Nikitochkin <michael@jetthoughts.com>
2019-02-07 12:48:48 +0100

    Certificates update on san changes.

--- modules/k8s_certificates/process.tf
+++ modules/k8s_certificates/process.tf
@@ -1,4 +1,8 @@
 resource "null_resource" "generate_pki" {
+  triggers {
+    cert_sans = "${var.cert_sans}"
+  }
+
   provisioner "local-exec" {
     command = "${path.module}/data/generate.sh ${var.name} ${var.kube_version} ${var.certs_path} '${var.cert_sans}'"
   }
@@ -6,6 +10,11 @@ resource "null_resource" "generate_pki" {
 
 resource "null_resource" "extract_admin_key" {
   depends_on = ["null_resource.generate_pki"]
+
+  triggers {
+    policy_sha1 = "${sha1(file("${var.certs_path}/pki/ca.crt"))}"
+  }
+
   provisioner "local-exec" {
     command = "ruby ${path.module}/data/extract_crt.rb -s ${var.certs_path}/admin.conf -d ${var.certs_path}"
   }
