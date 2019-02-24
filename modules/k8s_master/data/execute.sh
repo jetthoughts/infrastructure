2a2930705a36b82beaac319a006bb2d6c0df3075
Michael Nikitochkin <michael@jetthoughts.com>
2019-02-24 08:53:06 +0100

    Enable kubelet for master

--- modules/k8s_master/data/execute.sh
+++ modules/k8s_master/data/execute.sh
@@ -7,7 +7,7 @@ chmod +x /tmp/terraform/*.sh
 sudo /tmp/terraform/pre_init_script.sh
 sudo /tmp/terraform/packages.sh
 sudo /tmp/terraform/kube_packages.sh
-#sudo /tmp/terraform/k8s_kubelet_extra_args.sh
+sudo /tmp/terraform/k8s_kubelet_extra_args.sh
 sudo /tmp/terraform/certificates.sh
 sudo /tmp/terraform/kubeadm_config.sh
 sudo /tmp/terraform/master.sh || exit
--- modules/k8s_master/variables.tf
+++ modules/k8s_master/variables.tf
@@ -128,8 +128,6 @@ variable "kubelet_extra_args" {
 
   default = [
     "--cloud-provider=aws",
-    "--runtime-cgroups=/systemd/system.slice",
-    "--kubelet-cgroups=/systemd/system.slice",
   ]
 }
 
