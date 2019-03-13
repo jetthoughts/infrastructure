b066bee99db2969f452365dacd1e85940d590aa8
Michael Nikitochkin <michael@jetthoughts.com>
2019-03-13 09:10:28 +0100

    K8S use kube slug

--- modules/k8s_node/iam.tf
+++ modules/k8s_node/iam.tf
@@ -1,19 +1,19 @@
 // Nodes
 # IAM Profile to add access to cloud-provider features.
 resource "aws_iam_instance_profile" "nodes" {
-  name = "k8s_${var.name}_nodes"
+  name = "kube_${var.name}_nodes"
   role = "${aws_iam_role.nodes.name}"
-  path = "/k8s/${var.name}/"
+  path = "/kube/${var.name}/"
 }
 
 resource "aws_iam_role" "nodes" {
-  name               = "k8s_${var.name}_nodes"
+  name               = "kube_${var.name}_nodes"
   assume_role_policy = "${file("${path.module}/data/aws_iam_role_policy")}"
-  path               = "/k8s/${var.name}/"
+  path               = "/kube/${var.name}/"
 }
 
 resource "aws_iam_role_policy" "nodes" {
-  name   = "k8s_${var.name}_nodes"
+  name   = "kube_${var.name}_nodes"
   role   = "${aws_iam_role.nodes.name}"
   policy = "${file("${path.module}/data/aws_iam_role_policy_nodes_policy")}"
 }
