f658d2f0d51c576ce8193e58960eedda54d5efe4
Michael Nikitochkin <michael@jetthoughts.com>
2019-03-26 21:02:31 +0100

    Ignore kernel version

--- modules/k8s_node/data/node_join.tpl.sh
+++ modules/k8s_node/data/node_join.tpl.sh
@@ -21,7 +21,7 @@ docker ps
 
 docker pull gcr.io/google_containers/kube-proxy-amd64:${kube_version} || true
 
-kubeadm join --token="${kube_token}" ${master_ip}:6443 --node-name="$PRIVATE_HOSTNAME" --discovery-token-unsafe-skip-ca-verification
+kubeadm join --token="${kube_token}" ${master_ip}:6443 --node-name="$PRIVATE_HOSTNAME" --discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=SystemVerification
 
 export KUBELET_PATH="/etc/kubernetes/kubelet.conf"
 
